5d2cc137eaacbe8a13a50d85cee80548bc299829
northernjamie <northernjamie@gmail.com>
2017-03-15 16:23:49 +0000

    Working Version

--- server.R
+++ server.R
@@ -5,19 +5,139 @@
 # http://shiny.rstudio.com
 #
 
-library(shiny)
+library(shiny) ; library(dplyr) ; library(rgdal) ; library(leaflet) ; library(raster) ; library(SPARQL) ; library(DT) ; library(reshape2) ; library(ggplot2)
 
-shinyServer(function(input, output) {
+endpoint <- "http://ons.publishmydata.com/sparql"
+scotpayquery <- "PREFIX qb: <http://purl.org/linked-data/cube#>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+SELECT ?s ?value ?areaname ?sexname ?areacode ?statname WHERE {
+?s qb:dataSet <http://statistics.data.gov.uk/data/annual-survey-of-hours-and-earnings-2016-earnings> ;
+<http://statistics.data.gov.uk/def/measure-properties/value> ?value ;
+<http://statistics.data.gov.uk/def/dimension/earnings> <http://statistics.data.gov.uk/def/concept/earnings/annual-pay-gross>;
+<http://purl.org/linked-data/sdmx/2009/dimension#refArea> ?area;
+<http://statistics.data.gov.uk/def/dimension/earningsStatistics> ?statcode;
+<http://purl.org/linked-data/sdmx/2009/dimension#sex> ?sex;
+<http://statistics.data.gov.uk/def/dimension/workingPattern> <http://statistics.data.gov.uk/def/concept/working-pattern/full-time> .
+?area <http://statistics.data.gov.uk/def/statistical-entity#code> <http://statistics.data.gov.uk/id/statistical-entity/S12> ;
+<http://statistics.data.gov.uk/def/statistical-geography#officialname> ?areaname .
+?sex rdfs:label ?sexname .
+?area rdfs:label ?areacode .
+?statcode rdfs:label ?statname .
+}"
 
-  output$distPlot <- renderPlot({
+# SPARQL is too big to run succesfully
+#qd <- SPARQL(endpoint,query)
 
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2]
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+# Load the constituency geography file
+scotcouncil <- readOGR("ScottishCouncilAreas2_simplified.geojson", "OGRGeoJSON")
 
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
+# Load the csv file containing the pay data
+pgdata <- read.csv2("ons_ashe_scot_paygap.csv",header = TRUE, sep=",")
 
+#Turn the value column into from scientific notation to number
+pgdata <- transform(pgdata, value = as.numeric(value))
+
+pgdata <- as.data.frame(pgdata)
+pgdata2 <- pgdata[ which(pgdata$statname == "Median"), ]
+#pgdata <- subset(pgdata, year == "2016" & stat == "Median")
+pgdata2 <- dcast(pgdata2,areaname + areacode ~ sexname, value.var = 'value')
+pgdata2$gap <- with(pgdata2,Male - Female)
+
+#pgdist <- pgdata[ which(pgdata$year == "2016"), ]
+
+pgdist2 <- dcast(pgdata,areaname + areacode + statname ~ sexname, value.var = 'value')
+
+pgdist2$gap <- with(pgdist2, Male - Female)
+
+#Pluck the national figure out of the data
+pgdist2Nat <- pgdist2[ which(pgdist2$areacode == "K03000001"),]
+
+
+
+server <- (function(input, output, session) {
+  
+  # This bit is the bit that responds to the slider on the UI
+  #selected <- reactive({
+  # subset(smokpreg,
+  #        percsmoking < input$smokerange[2] & percsmoking >= input$smokerange[1])
+  #})
+  
+  selected <- reactive({
+    subset(pgdata2,
+           gap < input$upper & gap >= input$lower)
   })
-
+  
+  # Put the default map co-ordinates and zoom level into variables
+  lat <- 54.542788
+  lng <- -3.144708
+  zoom <- 6
+  
+  # Draw the map
+  output$map <- renderLeaflet({
+    
+    leaflet() %>% 
+      #maybe set a more colourful map background..?
+      addProviderTiles("Esri.WorldStreetMap") %>% 
+      setView(lat = lat, lng = lng, zoom = zoom) %>%
+      addPolygons(data = scotcouncil, opacity=1, color = "black", weight = 1, fillOpacity=0.5, layerId = scotcouncil$CODE)
+    
+  })
+  
+  # Draw the table (the columns need to match with all those in selected())
+  output$table <- DT::renderDataTable({
+    data.frame(x=selected())}, colnames = c('areaname','areacode','All','Female','Male','gap'), options = list(order = list(7,'desc')))
+  
+  #
+  observe({
+    
+    #merge the data from the csv / sparql with the geojson data for mapping
+    scotcouncil@data <- left_join(scotcouncil@data, selected(), by=c("CODE"="areacode"))
+    
+    #sets the colour range to be used on the choropleth
+    qpal <- colorNumeric("Spectral", pgdata2$gap, na.color = "#bdbdbd")
+    
+    #the popup on the map
+    popup <- paste0("<h5>",scotcouncil$NAME,"</h5><br /><h3>£",scotcouncil$gap,"</h3>")
+    
+    #draw the map with stuff on
+    leafletProxy("map", data = scotcouncil) %>%
+      addProviderTiles("Esri.WorldStreetMap") %>% 
+      clearShapes() %>% 
+      clearControls() %>% 
+      addPolygons(data = scotcouncil, fillColor = ~qpal(gap), fillOpacity = 0.7, 
+                color = "#bdbdbd", weight = 1, popup = popup, layerId = scotcouncil$CODE) %>%
+      addLegend(pal = qpal, values = ~gap, opacity = 0.7,
+                position = 'bottomleft', 
+                title = paste0("The Pay Gap"))
+  })
+  
+  observe({
+    input$reset_button
+    leafletProxy("map") %>% setView(lat = lat, lng = lng, zoom = zoom)
+  })
+  
+  observe({
+    click<-input$map_shape_click
+    if(is.null(click))
+      return()
+   
+    available <- pgdata2[ which(pgdata2$areacode == click$id), ]
+    
+    text2 <- paste0("Constituency: ", available[1,1], " (", available[1,2],")")
+    
+    #text2<-paste("You've selected shape: ", click$id)
+    output$const_name<-renderText({
+      text2
+    })
+    
+    pgdist2Const <- pgdist2[ which(pgdist2$areacode == available[1,2]),]
+    output$plot1 <- renderPlot({
+      ggplot() + geom_bar(data = pgdist2Const, aes(x=statname,y=All), stat="identity") + geom_point(data = pgdist2Nat, aes(x=statname,y=All), stat="identity")
+    })
+    
+    
+  })
+  
+  
+  
 })
--- ui.R
+++ ui.R
@@ -5,26 +5,111 @@
 # http://shiny.rstudio.com
 #
 
-library(shiny)
+library(shiny) ; library(dplyr) ; library(rgdal) ; library(leaflet) ; library(raster) ; library(SPARQL) ; library(DT); library(ggplot2)
 
-shinyUI(fluidPage(
+# shinyUI(fluidPage(
+#   fluidRow(
+#     column(7, offset = 1,
+#            br(),
+#            div(h3("Gender Pay Gap")),
+#            div(h4(textOutput("const_name"))),
+#            div(h4("The gap between male and female pay by parliamentary constituency")),
+#            div(h4(textOutput("title"), align = "center"), style = "color:black"),
+#            div(h5(textOutput("period"), align = "center"), style = "color:black"),
+#            br())),
+#   fluidRow(
+#     column(7, offset = 1,
+#            tabsetPanel(
+#              tabPanel("Map",leafletOutput("map", height="600"),
+#                       br(),
+#                       actionButton("reset_button", "Reset view")),
+#              tabPanel("Table",DT::dataTableOutput("table"))
+#            )
+#     ),
+#     column(3,
+#            sliderInput("smokerange", "Choose the range of values you would like to display", min = -5000, max = 20000, value = c(-5000,20000)),
+#            plotOutput("plot1"),
+#            br()))
+#   
+# ))
 
-  # Application title
-  titlePanel("Old Faithful Geyser Data"),
-
-  # Sidebar with a slider input for number of bins
-  sidebarLayout(
-    sidebarPanel(
-      sliderInput("bins",
-                  "Number of bins:",
-                  min = 1,
-                  max = 50,
-                  value = 30)
-    ),
-
-    # Show a plot of the generated distribution
-    mainPanel(
-      plotOutput("distPlot")
-    )
-  )
-))
+navbarPage("ONS Paygap Demo", id="nav",
+           
+           tabPanel("MAP",
+                    div(class="outer",
+                        
+                        tags$head(
+                          # Include our custom CSS
+                          includeCSS("styles.css"),
+                          includeScript("gomap.js")
+                        ),
+                        
+                        leafletOutput("map", width="100%", height="100%"),
+                        
+                        # Shiny versions prior to 0.11 should use class="modal" instead.
+                        absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
+                                      draggable = TRUE, top = 60, left = "auto", right = 20, bottom = "auto",
+                                      width = 450, height = "auto",
+                                      
+                                      h2("Paygap Data"),
+                                      h5("Filter constituencies by the gap between male and female median pay"),
+                                      #sliderInput("paygaprange", "Choose the range of values you would like to display", min = -5000, max = 20000, value = c(-5000,20000)),
+                                      numericInput("lower", "Paygap between", value= -5000),
+                                      numericInput("upper", "and", value=20000),
+                                      fluidRow(
+                                        column(6,plotOutput("plot1"))#,
+                                        #column(6,plotOutput("plot2"))
+                                      )
+                                      
+                                      
+                                      
+                                      
+                                      #selectInput("color", "Color", vars),
+                                      #selectInput("size", "Size", vars, selected = "adultpop"),
+                                      # conditionalPanel("input.color == 'superzip' || input.size == 'superzip'",
+                                      #                  # Only prompt for threshold when coloring or sizing by superzip
+                                      #                  numericInput("threshold", "SuperZIP threshold (top n percentile)", 5)
+                                      # ),
+                                      
+                                      #plotOutput("histCentile", height = 200),
+                                      #plotOutput("scatterCollegeIncome", height = 250)
+                        ),
+                        
+                        tags$div(id="cite",
+                                 'Data compiled for ', tags$em('Coming Apart: The Fucking Absolute State of the UK, 2017'), ' by Jamie Whyte (Crown Forum, 2012).'
+                        )
+                    )
+           ),
+           
+           tabPanel("DATA",
+                    fluidRow(
+                      column(3,
+                             div(h3("Gender Pay Gap"))
+                      ),
+                      column(3,
+                             div(h3("Gender Pay Gap"))
+                      ),
+                      column(3,
+                             div(h3("Gender Pay Gap"))
+                      )
+                    ),
+                    fluidRow(
+                      column(1,
+                             div(h3("Gender Pay Gap"))
+                      ),
+                      column(1,
+                             div(h3("Gender Pay Gap"))
+                      )
+                    )
+           ),
+           
+           tabPanel("ABOUT",
+                    fluidRow(
+                      column(1,
+                             div(h5("About this tool"))
+                             )
+                      )
+                    ),
+           
+           conditionalPanel("false", icon("crosshair"))
+)
